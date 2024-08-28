186b1219fd77e637c6d7ec242dca1f96ea4b9fb5
Kwasi633 <ezrakwasi633@gmail.com>
2024-08-28 10:09:10 +0000

    Update project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose")
+
+const HttpError = require('./models/http-error')
+const userRoutes = require('./routes/users-routes')
+
+const app = express();
+
+app.use(bodyParser.json())
+
+
+app.use('/api/users/', userRoutes)
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route', 404)
+    throw error
+})
+
+app.use((error, req, res, next) => {
+    if (res.headerSent){
+        return next(error)
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occured!'})
+})
+
+// mongoose
+//     .connect('mongodb+srv://SafePathAdmin:f9wlMkIbGmnCnBtu@cluster0.keud0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+//     .then(() => {
+//         app.listen(6000)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     });
+
+mongoose
+    .connect('mongodb+srv://backendAdmin:SDACTDBTQAbQrit6@cluster0.4epjd.mongodb.net/user-account?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        app.listen(5050)
+    })
+    .catch(err => {
+        console.log(err)
+});
